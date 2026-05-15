00c14910952b3e9a8bd847dbc418cca33dd90192
KinhKhaVN <pebin0123@gmail.com>
2026-05-15 20:22:10 +0700

    Simulate for user input
    - Compute 01: ignore error
    - Compute 02: Anounce error but just "Error"
    - Compute 03: More detail for error
    - Compute 04: Full error (No input, {number} is a perfect square or not

--- OptionT/src/main.rs
+++ OptionT/src/main.rs
@@ -1,21 +1,68 @@
-use std::result;
+fn get_input() -> Option<String> {
+    Some("x26".to_string())
+}
 
-fn divine(a: i32, b: i32) -> Option<i32> {
-    if b == 0 {
-        None
-    }
-    else {
-        Some(a / b)
-    }
+fn parse(s: &str) -> Option<i32> {
+    s.parse::<i32>().ok()
+}
+
+fn perfect_square(n: i32) -> Option<i32> {
+
+    (0..=n).find(|x| x * x == n)
+}
+
+fn compute_01() -> Option<i32> {
+    let input = get_input()?;
+    let number = parse(&input)?;
+    perfect_square(number)
+}
+
+fn compute_02() -> Option<i32> {
+    get_input()
+        .and_then(|input| parse(&input))
+        .and_then(perfect_square)
+}
+
+fn compute_03() -> Result<i32, String> {
+    let input = get_input().ok_or("No input".to_string())?;
+    let number = parse(&input).ok_or("Not a number".to_string())?;
+    perfect_square(number).ok_or("Not a perfect square".to_string())
+}
+
+fn compute_04() -> Result<i32, String> {
+    let input = get_input().ok_or("No input".to_string())?;
+
+    let number = parse(&input).ok_or_else(|| {
+        let mut msg = String::new();
+        core::fmt::write(
+            &mut msg,
+            format_args!("{input} is not a number!")).unwrap();
+        msg
+    })?;
+    perfect_square(number).ok_or("Not a perfect square".to_string())
 }
 
 fn main() {
 
-    match divine(10, 0) {
-        Some(result) => println!("Result: {}", result),
-        None              => println!("Divine by zero!")
+    if let Some(r) = compute_01() {
+        println!("{r} is a perfect square")
     }
 
-    let result = divine(10, 0).unwrap_or(-1);
-    println!("Result: {}", result);
+    println!("-----compute_02-----");
+    match compute_02() {
+        Some(r) => println!("{r} is a perfect square"),
+        None         => println!("Error!")
+    }
+
+    println!("-----compute_03-----");
+    match compute_03() {
+        Ok(r) => println!("{r}"),
+        Err(e)         => println!("{e}")
+    }
+
+    println!("-----compute_04-----");
+    match compute_04() {
+        Ok(r) => println!("{r}"),
+        Err(e)         => println!("{e}")
+    }
 }
